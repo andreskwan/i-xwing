afcf67ff9613b4687cd57609e34078c4dfa8f1dd
ANDRES KWAN <andreskwan@gmail.com>
2013-05-09 13:26:47 -0500

    agbo-ex6-animation

--- AgboXwingIphone/KWViewController.h
+++ AgboXwingIphone/KWViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface KWViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *spaceView;
+@property (weak, nonatomic) IBOutlet UIImageView *xwingView;
 
+-(void) onTap:(UITapGestureRecognizer *)tap;
 @end
--- AgboXwingIphone/KWViewController.m
+++ AgboXwingIphone/KWViewController.m
@@ -14,6 +14,58 @@
 
 @implementation KWViewController
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    //creamos un detector de tap
+    UITapGestureRecognizer * tap = [[UITapGestureRecognizer alloc]
+                                    initWithTarget:self action:@selector(onTap:)];
+    //lo anadimos
+    [self.view addGestureRecognizer:tap];
+}
+
+-(void) onTap: (UITapGestureRecognizer *) tap
+{
+    if (tap.state == UIGestureRecognizerStateRecognized) {
+        CGPoint location = [tap locationInView:self.spaceView];
+    
+        
+        //cambiar la posicion de las vista del xwing
+        UIViewAnimationOptions options =
+        UIViewAnimationOptionBeginFromCurrentState |
+        UIViewAnimationOptionCurveEaseInOut;
+        
+        [UIView animateWithDuration: 1.9
+                              delay:0
+                            options:options
+                         animations:^{
+                                self.xwingView.center = location;
+                            }
+                         completion:^(BOOL finished) {
+                                //
+                            }];
+        //roacion
+        [UIView animateWithDuration:0.7
+                              delay:0
+                            options:options
+                         animations:^{
+                             //cambio de transformada
+                             self.xwingView.transform = CGAffineTransformMakeRotation(M_2_PI);
+                         }
+                         completion:^(BOOL finished){
+                             //dejamos la transformada como estaba
+                             [UIView animateWithDuration:0.7
+                                                   delay:0
+                                                 options:options
+                                              animations:^{
+                                                  self.xwingView.transform = CGAffineTransformIdentity;
+                                              }
+                                              completion:nil];
+                             
+                         }];        
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
